3f42f4fd6ff28917e5a9c38a6d0b7275c0540a1f
King <mac@mac.com>
2024-07-24 06:23:58 +0900

    chapter 05

--- chapter02/src/App.jsx
+++ chapter02/src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css'
+import {useEffect, useState} from "react";
+
 
 function App() {
 
-  return (
-    <>
-    </>
-  )
+    const [isLoading, setIsLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 6000);
+    }, []);
+
+    return (<div className="App">
+        {isLoading ? 'Loading...' : 'We are ready'}
+    </div>)
 }
 
 export default App
---
+++ chapter02/src/train.js
@@ -0,0 +1,8 @@
+
+const friends = ["dal","mark","lynn","kim"];
+
+ const modify = friends.map((friend) => {
+    return `${friend} 😁`;
+});
+
+ console.log(modify);
